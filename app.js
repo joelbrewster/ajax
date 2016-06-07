037f7f8115f90fa7ff19f8471de41a6ed33f7264
Joel <j.n.brewster@gmail.com>
2016-06-07 16:06:24 +1000

    reddit with prev and next

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-$(function() {
-  $.ajax({
-    url:'https://www.reddit.com/r/funny.json',
-    method:'GET',
-    success: function(data){
-      console.log(data);
-      for (var i = 0; i < data.data.children.length; i++) {
-        var post = data.data.children[i];
-        $('body').append($('<p>').text(post.data.title));
+$(function(){
+
+  function getRedditPosts(redditID){
+    var _url: 'https://www.reddit.com/r/funny.json?count=12';
+    _url += redditID == undefined ? '' : '$after=' + redditID;
+    $.ajax({
+      url:_url
+      method: 'GET',
+      success: function(data){
+        for (var i = 0; i < data.data.children.length; i++) {
+          var post = data.data.children[i];
+          // console.log(post.data.preview.images[0].resolutions[0].url);
+          var postDiv = $('<div>')
+          .addClass('post').data('reddit-id', post.data.name);
+          postDiv.append($('<p>').text(post.data.title).attr("id", i+1));
+          $('#posts').append(postDiv);
+
+          var postThumb = $('<img>').attr("src", post.data.thumbnail);
+          postDiv.append(postThumb);
+        }
+      },
+      error: function(data){
+        console.log(data);
       }
-    },
-    error: function(data){
-      console.log(data);
-    },
+    });
+  }
+  getRedditPosts();
+
+  $('#next').on('click', function() {
+    // Load the next set of posts from reddit
+    var lastID = $('.post:last-of-type').data('reddit-id');
   });
-
-  // var request = new XMLHttpRequest();
-  // //request.open ("PROTOCOL", "URL");
-  // request.open("GET", "https://www.reddit.com/r/funny.json");
-  // request.send();
-
-
-  // request.onreadystatechange = function(){
-  //   console.log("Ready State: " + request.readyState);
-  //   if (request.readyState === 4) {
-  //     var data = JSON.parse(request.responseText);
-  //     console.log(data);
-  //   }
-  // }
-
 });
