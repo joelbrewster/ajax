2c1d8148625901ef49325a5db17df7137d65a65e
Joel <j.n.brewster@gmail.com>
2016-06-10 12:19:55 +1000

    Git init

--- app.js
+++ app.js
@@ -1,33 +1,75 @@
-$(function(){
+var page = 0;
+var loading = false;
+function getRedditPost(redditId,direction){
 
-  function getRedditPosts(redditID){
-    var _url: 'https://www.reddit.com/r/funny.json?count=12';
-    _url += redditID == undefined ? '' : '$after=' + redditID;
-    $.ajax({
-      url:_url
-      method: 'GET',
-      success: function(data){
-        for (var i = 0; i < data.data.children.length; i++) {
-          var post = data.data.children[i];
-          // console.log(post.data.preview.images[0].resolutions[0].url);
-          var postDiv = $('<div>')
-          .addClass('post').data('reddit-id', post.data.name);
-          postDiv.append($('<p>').text(post.data.title).attr("id", i+1));
-          $('#posts').append(postDiv);
+  var _url = 'https://www.reddit.com/r/jokes.json?count=6';
+  // if (redditId == undifined){
+    //   _url += '';
+    // }else {
+      //   _url += '&after=' + redditId;
+      // }
+      // same as line below
+      _url += redditId == undefined ? '' : '&'+direction+'=' +redditId;
+      $.ajax({
+        url: _url,
+        method:'GET',
+        success: function(data){
+          // $("#title").empty();
+          $("#posts").empty();
+          for(var i = 0; i < data.data.children.length; i++){
+            var post =data.data.children[i];
+            var postDiv = $('<div>').addClass('post')
+            .data('reddit-id', post.data.name);
 
-          var postThumb = $('<img>').attr("src", post.data.thumbnail);
-          postDiv.append(postThumb);
+            // to call $('.post.eq(0)').data(reddit-id)
+
+            postDiv.append($('<p>').text(post.data.title));
+            postDiv.append($('<p>').text('------------------------------------------------------------'));
+            postDiv.append($('<p>').text(post.data.selftext));
+            // postDiv.append($('<p>').text(post.data.author));
+            // postDiv.append($('<p>').text(post.data.created));
+            if(post.data.thumbnail && post.data.thumbnail.indexOf('.jpg') != -1){
+              postDiv.append($('<img>').attr('src', post.data.thumbnail));
+            }
+
+            $('#posts').append(postDiv);
+
+            // $('#title').append($('<p>').text(post.data.title));
+            // $('#title').append($('<p>').text("----------------------------------------"));
+            // $('#posts').append($('<div>').text(post.data.selftext));
+            // $('#posts').append($('<p>').text("========================================"));
+          }
+          loading = false;
+        },
+        error: function(data){
+          console.log(data);
         }
-      },
-      error: function(data){
-        console.log(data);
+      });
+    }
+    var loading = true;
+    getRedditPost(undefined);
+
+    $('#next').on('click', function(){
+      console.log('shit');
+      //load next set of posts
+      if(!loading){
+        loading = true
+        var lastId = $('.post:last-of-type').data('reddit-id');
+        page++;
+        $('#previous').show();
+        getRedditPost(lastId, 'after');
       }
     });
-  }
-  getRedditPosts();
-
-  $('#next').on('click', function() {
-    // Load the next set of posts from reddit
-    var lastID = $('.post:last-of-type').data('reddit-id');
-  });
-});
+    $('#previous').on('click', function(){
+      console.log('shit');
+      //load previous set of posts
+      if(!loading){
+        loading = true
+        var firstId = $('.post:first-of-type').data('reddit-id');
+        page--;
+        if (page == 0){
+          $(this).hide();
+        }
+        getRedditPost(firstId, 'before');
+      }
+    });
